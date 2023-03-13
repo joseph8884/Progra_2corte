98620a0700a2f49609fa39064434dc095efd40d6
Santiago Santafe <andressasi@unisabana.edu.co>
2023-03-13 13:16:32 -0500

    Agrego entrada numero2

--- raiz.java
+++ raiz.java
@@ -1,7 +1,7 @@
 import javax.swing.JOptionPane;
 
 public class raiz {
-    public void raizde2num(int numero1) {
+    public void raizde2num(int numero1,int numero2) {
         Math.sqrt(numero1);
     }
 }
