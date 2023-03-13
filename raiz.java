cd21c6c9f5d44e7011db0e33577faf501e2e5ec7
joseph8884 <josegzm08@gmail.com>
2023-03-13 13:24:16 -0500

    Agrege la funcionalidad de elevar al cuadrado y imprimo en consola el resultado de la raiz

--- raiz.java
+++ raiz.java
@@ -2,6 +2,9 @@ import javax.swing.JOptionPane;
 
 public class raiz {
     public void raizde2num(int numero23) {
-        Math.sqrt(numero23);
+        System.out.println(Math.sqrt(numero23));
+    }
+    public void elevarCuadrado(int numero1, int numero2){
+        System.out.println(Math.pow(numero1,numero2));
     }
 }
